1b10789aa2cf4538eaf34c009f6845e15abf7573
Парамонов Никита Сергеевич <paramonov_n@tochka.com>
2021-12-05 19:14:02 +0500

    API, этап II

--- API.py
+++ API.py
@@ -9,10 +9,9 @@ def get_datetime(commit_info):
     return datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ').date()
 
 
-url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%93%D1%80%D0' \
-      '%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%91%D0%BE%D1%80' \
-      '%D0%B8%D1%81%D0%BE%D0%B2%D0%B8%D1%87 '
-page = "183903"
+url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%91%D0%B5%D0' \
+      '%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE,_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C '
+page = "192203"
 
 data = loads(urlopen(url).read().decode('utf8'))
 page_info = data['query']['pages'][page]['revisions']
